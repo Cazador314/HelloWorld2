48d55e80ed50df8ba764cdff318b5bf70efae426
Christ Hunter <christopher.hunter1@maine.edu>
2018-02-04 22:42:56 -0500

    Hey Charlie

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -3,6 +3,7 @@ public class helloWorld {
 
    public static void main(String[] args) {
 
+      System.out.println("Hey Charlie");
       System.out.println("Hi Alice and Bob");
       System.out.println("Hello World!");
    }
